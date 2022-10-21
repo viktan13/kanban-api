cedab2980eab022fca5991d206139ab48fdf1281
Viktor <vik.tan@hotmail.com>
2022-10-20 19:26:09 -0500

    deleted cards object

--- routes.js
+++ routes.js
@@ -2,11 +2,6 @@ const cardCreate = require('./cards/cardCreate');
 const cardGetAll = require('./cards/cardGetAll');
 const cardUpdateByID = require('./cards/cardUpdateByID');
 const cardDelete = require('./cards/cardDelete');
-let cards = [
-    { id: '1', name: 'First Card', status: 'todo', priority: 2 },
-    { id: '2', name: 'Second Card', status: 'progress', priority: 5 },
-    { id: '3', name: 'Third Card', status: 'review', priority: 10 },
-];
 
 function routes(app) {
     app.get('/', (req, res) => {
